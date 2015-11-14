bbf06f0f2ae813366947e94fd192ac9069299657
kobe2571 <fdzxas@gmail.com>
2015-11-14 18:48:09 +0800

    Java Pass HTTPS Verification

---
+++ Java绕过 https 方法/https_codes/HttpClientHelper.java
@@ -0,0 +1,68 @@
+package com.cycst.tickets.https;
+
+import java.security.KeyStore;
+import org.apache.http.HttpVersion;
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.params.ConnManagerParams;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.HTTP;
+
+public class HttpClientHelper {
+	
+	private static HttpClient client;
+	
+	public HttpClientHelper() {
+		
+	}
+	
+	public static synchronized HttpClient getHttpClient()
+	{
+		if(null == client)
+		{
+			try {
+				KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());  
+	            trustStore.load(null, null);  
+	            
+	            SSLSocketFactory sf = new SSLSocketFactoryEx(trustStore);  
+	            sf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);  	// 允许所有
+	    
+	            HttpParams params = new BasicHttpParams();
+	            
+	            HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);  
+	            HttpProtocolParams.setContentCharset(params, HTTP.DEFAULT_CONTENT_CHARSET);
+	            HttpProtocolParams.setUseExpectContinue(params, true);
+	            
+	            // 设置连接管理器的超时
+	            ConnManagerParams.setTimeout(params, 10000);
+	            // 设置连接超时
+	            HttpConnectionParams.setConnectionTimeout(params, 10000);
+	            // 设置socket超时
+	            HttpConnectionParams.setSoTimeout(params, 10000);
+	    
+	            // 设置http https支持
+	            SchemeRegistry registry = new SchemeRegistry();  
+	            registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));  
+	            registry.register(new Scheme("https", sf, 443));  
+	    
+	            ClientConnectionManager ccm = new ThreadSafeClientConnManager(params, registry);  
+	    
+	            client = new DefaultHttpClient(ccm, params);
+	            
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return client;
+	}
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Java绕过 https 方法
+
+####重新封装SSLSocketFactory，绕过HTTPS的CA证书认证
+
+####封装HttpClientHelper
